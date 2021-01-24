c746706aced5f6e18f375e15e51f891c332344eb
Jeff Labonte <grimsleepless@protonmail.com>
2021-01-24 16:34:21 -0500

    Complete base to handle queries

--- src/controllers/script_controller.py
+++ src/controllers/script_controller.py
@@ -1,6 +1,17 @@
 import flask
 
+
 def handle_script_create(request: flask.request):
     pass
 
 
+def handle_script_retrive(request: flask.request):
+    pass
+
+
+def handle_script_update(request: flask.request):
+    pass
+
+
+def handle_script_delete(request: flask.request):
+    pass
