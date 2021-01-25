49e29cfb2ae97b1e2867a36d27e650882136fd7f
Jeff Labonte <grimsleepless@protonmail.com>
2021-01-24 23:11:10 -0500

    Update controller to raise Exception on wrong payload

--- src/controllers/script_controller.py
+++ src/controllers/script_controller.py
@@ -1,7 +1,15 @@
 import flask
 
+from common.exceptions import InvalidPayloadException
+from common.validator import validate_schema
+from schemas.script import script_post_schema
+
 
 def handle_script_create(request: flask.request):
+    payload = request.json
+    if errors := validate_schema(payload, script_post_schema):
+        raise InvalidPayloadException(f'Errors: {errors}')
+
     return ''
 
 
