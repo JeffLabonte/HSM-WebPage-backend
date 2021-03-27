296d1e130c2e9de1b7814cd0d0d89c62cf9b4106
Jeff Labonte <grimsleepless@protonmail.com>
2021-03-27 19:25:13 -0400

    First draft of ScriptWriteSerializer

--- src/backend/api/scripts/serializers.py
+++ src/backend/api/scripts/serializers.py
@@ -1,5 +1,26 @@
+from uuid import uuid4
+
 from rest_framework import serializers
 
 
 class ScriptWriteSerializzer(serializers.Serializer):
     data = serializers.CharField(required=True, write-only=True)
+    transaction_id = serializers.CharField(required=True, default=uuid4())
+    method = serializers.ChoiceField(choices=[
+        ("PUT", "PUT"),
+        ("POST", "POST"),
+        ("PATCH", "PATCH"),
+    ])
+
+
+    class Meta:
+        fields = (
+            "data",
+            "transaction_id",
+            "method",
+        )
+        default_fields = (
+            "data",
+            "transaction_id",
+            "method"
+        )
