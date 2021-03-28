77cadf69fde4a653abb756d4ce4607d9e66855d7
Jeff Labonte <grimsleepless@protonmail.com>
2021-03-28 01:28:28 -0400

    Begin work in ScirptCreateView post

--- src/backend/api/scripts/views.py
+++ src/backend/api/scripts/views.py
@@ -1,6 +1,18 @@
+from backend.api.scripts.serializers import ScriptWriteSerializer
+
+from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
 
 
 class ScriptCreateView(APIView):
     def post(self, request, format=None):
-        data = request.data
+        serializer = ScriptWriteSerializer(data=request.data)
+        print(serializer)
+        if serializer.is_valid():
+            # TODO Change for something like `audited_send` so it is implicit that I am saving and sending
+            serializer.save()
+            # TODO Add call to get return
+            # Use something else to save()?
+            return Response(serializer.data, status=HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
